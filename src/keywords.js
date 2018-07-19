fd89334a80c708add942087d5bea8edebf8c9070
wanghc <wanghuicai123@gmail.com>
2018-07-19 15:36:21 +0800

    增加关键字列表功能

---
+++ src/keywords.js
@@ -0,0 +1,130 @@
+(function($){
+	function getLabelItem(target, labelid){
+		var opts = $.data(target, 'keywords').options;
+		var items = opts.items;
+		var labelidArr = labelid.split("-");
+		if (labelidArr.length==1){
+			return items[labelid[0]];
+		}
+		if(labelidArr.length==2){
+			return items[labelidArr[0]].items[labelidArr[1]];
+		}
+		if (labelidArr.length==3){
+			return items[labelidArr[0]].items[labelidArr[1]].items[labelidArr[2]];
+		}
+	}
+	function createKeywords(target){
+		var t = $(target).empty();
+		var opts = $.data(target, 'keywords').options;
+		if(opts.labelCls!='blue') t.addClass("keywords-label"+opts.labelCls);
+		var html = '';
+		$.each(opts.items,function(indc,chp){
+			if(chp.type=="chapter"){
+				html +='<div class="kw-chapter"><a></a>'+chp.text+'</div><div class="kw-line"></div>';
+				$.each(chp.items,function(inds,sec){
+					if(sec.type=='section'){
+						html +='<div class="kw-section"><div class="kw-section-header">'+sec.text+'</div>';
+						if (sec.items){
+							html += '<ul class="kw-section-list keywords">';
+						}
+						$.each(sec.items, function(indl,lbl){
+							var s = lbl.selected?'class="selected"':'';
+							html += '<li id="'+(lbl.id||lbl.text)+'" rowid="'+indc+'-'+inds+'-'+indl+'" '+s+'><a>'+lbl.text+'</a></li>'
+						});
+						if (sec.items){
+							html +='</ul>'
+						}
+						html += '</div>' //kw-section end
+					}else{ //默认label
+						if (inds==0) {html += '<ul class="kw-section-list keywords">';}
+						var s = sec.selected?'class="selected"':'';
+						html += '<li id="'+(sec.id||sec.text)+'" rowid="'+indc+'-'+inds+'" '+s+'><a>'+sec.text+'</a></li>';
+						if (inds==(chp.items.length-1)) html +='</ul>';
+					}
+				});
+			}else{
+				if (indc==0) {html += '<ul class="kw-section-list keywords">';}
+				var s = chp.selected?'class="selected"':'';
+				html += '<li id="'+(chp.id||chp.text)+'" rowid="'+indc+'" '+s+'><a>'+chp.text+'</a></li>';
+				if (indc==(opts.items.length-1)) html +='</ul>';
+			}
+		});
+		t.append(html);
+        t.on('click','ul.kw-section-list>li',function(e,value){
+			var id = $(this).attr('id');
+			selectById(target,id);
+			return false;
+        });
+	}
+	
+	$.fn.keywords = function(options, param){
+		if (typeof options == 'string'){
+			return $.fn.keywords.methods[options](this, param);
+		}
+		options = options || {};
+		return this.each(function(){
+			var state = $.data(this, 'keywords');
+			if (state){
+				$.extend(state.options, options);
+			} else {
+				$.data(this, 'keywords', {
+					options: $.extend({}, $.fn.keywords.defaults, $.fn.keywords.parseOptions(this), options)
+				});
+			}
+			createKeywords(this);
+		});
+	};
+	function selectById(t,id){
+		var target = $(t);
+		var opts = $.data(t, 'keywords').options;
+		if(opts.singleSelect){
+			target.find('li.selected').removeClass('selected');
+		}
+		var _t = target.find('li#'+id);
+		_t.toggleClass('selected');
+		var item = getLabelItem(t, _t.attr("rowid"));
+		opts.onClick.call(this, item);
+		if (!opts.singleSelect){ //单选不进入select与unselect事件
+			if(_t.hasClass('selected')){
+				opts.onUnselect.call(this,item);
+			}else{
+				opts.onSelect.call(this,item);
+			}
+		}
+	}
+	function getAllSelected(target){
+		var rs=[];
+		$(target).find('li.selected').each(function(){
+			rs.push(getLabelItem(target, $(this).attr("rowid")));
+		});
+		return rs;
+	}
+	$.fn.keywords.methods = {
+		options: function(jq){
+			return $.data(jq[0], 'keywords').options;
+		},
+		getSelected:function(jq){
+			return getAllSelected(jq[0]);
+		},
+		select:function(jq,id){
+			return selectById(jq[0],id);
+		}
+    };
+    
+	$.fn.keywords.parseOptions = function(target){
+		var t = $(target);
+		return $.extend({}, $.parser.parseOptions(target, 
+			['id','iconCls','iconAlign','group','size',{plain:'boolean',toggle:'boolean',selected:'boolean'}]
+		), {
+			disabled: (t.attr('disabled') ? true : undefined)
+		});
+	};
+	
+	$.fn.keywords.defaults = {
+		singleSelect:false,
+        labelCls:'blue', //red
+		onClick:function(value){},
+		onUnselect:function(value){},
+		onSelect:function(value){}
+	};
+})(jQuery);
