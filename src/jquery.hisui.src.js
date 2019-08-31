5a639c989d8b56fa945604763664e772a70e5e5c
wanghc <wanghuicai123@gmail.com>
2019-09-01 07:51:49 +0800

    datagrid 支持onColumnsLoad事件

--- src/jquery.hisui.src.js
+++ src/jquery.hisui.src.js
@@ -10296,6 +10296,7 @@ if (typeof JSON !== 'object') {
                 }
                 if(opts.queryName){ //从query中取cm
                     var json = getColumns(opts);
+                    if(opts.onColumnsLoad) opts.onColumnsLoad.call(_64f,json.cm);
                     opts.columns = [json.cm];
                     opts.pageSize = json.pageSize;
                     if(opts.pageList) opts.pageList.push(opts.pageSize);
@@ -11247,6 +11248,7 @@ if (typeof JSON !== 'object') {
         },onDblClickHeader:function(e,_739){    //cryze 双击表格头事件，默认
         },lazy:false    //cryze 2018-3-22 为true初始化不加载列表数据
         ,onHighlightRow:function(index,row){ //cryze datagrid 高亮行(鼠标悬浮和combogrid上下选时)触发事件
+        },onColumnsLoad:function(grid,cm){
         }
     });
 })(jQuery);
