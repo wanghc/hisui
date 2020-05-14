cf200ecd4325dec9eefb99d6cacbd7b81304a32c
unknown <wanghuicai123@gmail.com>
2020-05-14 10:48:25 +0800

    feature:合并护理的单选下拉

--- src/combogrid.js
+++ src/combogrid.js
@@ -296,3 +296,170 @@
         },lazy:false   //cryze 2018-3-22
     });
 })(jQuery);
+
+//不写API--只护理使用-下拉单选
+(function ($) {
+    function getRadioId(comboboxId,val)
+    {
+        return comboboxId + "_" + val;
+    }
+    function getRadioname(comboboxId) {
+        return comboboxId + "_name";
+    }
+    var methods = {
+        init: function (options) {
+            var defaults = {
+                required: false,
+               // editable: false,
+                Onchange: null,
+               // multiple: false
+                //   , valueField: "id"
+                //  , textField: "text"
+            }
+            var endOptions = $.extend(defaults, options);
+            this.each(function () {
+                var _this = $(this);
+                var id = _this.attr("id");
+                $(_this).combobox({
+                    valueField: endOptions.valueField,
+                    textField: endOptions.textField,
+                    multiple: false,
+                    allowNull:true,
+                    selectOnNavigation: false,
+                    anelHeight: "auto",
+                    editable: endOptions.editable,
+                    required: endOptions.required,
+                    data: endOptions.data,
+                    formatter: function (row) {
+                        var opts;
+                        var val = row[endOptions.valueField];
+                        var text = row[endOptions.textField];
+                        var RadioId = getRadioId(id, val);
+                        var Radioname = getRadioname(id);
+                        if (row.selected == true) {
+                            opts = "<input style='height:13px' type='radio' checked='checked' id='" + RadioId + "' name='" + Radioname + "' value='" + val + "'>" + text;
+                        } else {
+                            opts = "<input style='height:13px' type='radio' id='" + RadioId + "' name='" + Radioname + "' value='" + val + "'>" + text;
+                        }
+                        return opts;
+                    },
+                    onSelect: function (rec) {
+						if(rec)
+						{ 				
+							  var val = rec[endOptions.valueField];
+                        var RadioId = getRadioId(id, val);                  
+                        $("#" + RadioId).prop('checked', true);
+                        if (endOptions.Onchange) {
+                            endOptions.Onchange(rec);
+                        }
+						}
+                      
+                    }, onUnselect: function (rec) {
+                        var val = rec[endOptions.valueField];
+                        var RadioId = getRadioId(id, val);                 
+                        $("#" + RadioId).prop('checked', false);
+                      /*  if (endOptions.Onchange) {
+                            endOptions.Onchange(false, rec);
+                        }*/
+                    }
+                });
+            });
+        },
+        enable: function (isenable) {
+            var _this = $(this);
+            if (isenable) {
+                $(_this).combobox("enable");
+            }
+            else {
+                $(_this).combobox("disable");
+            }
+        },
+        disable: function (isenable) {
+            var _this = $(this);
+            if (isenable) {
+                $(_this).combobox("disable");
+            }
+            else {
+                $(_this).combobox("enable");
+            }
+        },
+        setValue: function (vals) {
+            var _this = $(this);
+            var id = _this.attr("id");
+            $(_this).combobox("setValue", vals);        
+            var RadioId = getRadioId(id, vals);        
+            $("#" + RadioId).prop('checked', true);
+			 var itemselect=null;
+			 	var valueField=$(_this).combobox('options').valueField;
+                  for (var i = 0; i < datas.length; i++)
+				{
+					if(datas[i][valueField]==vals[0])
+					{
+						itemselect=datas[i];
+						break;
+					}
+				}
+				if(itemselect!=null)
+				 $(_this).combobox('options').onSelect.call($('#' + id)[0], itemselect);
+ 
+        },
+        setValues: function (vals) {
+            var _this = $(this);
+            var id = _this.attr("id");//单选只能选择一个，
+            if (vals.length > 0) {
+                var tempval = vals[0];
+                $(_this).combobox("setValue", tempval);
+                var RadioId = getRadioId(id, tempval);
+                $("#" +RadioId).prop('checked', true);				
+				var datas=$(_this).combobox("getData");
+				var itemselect=null;
+				var valueField=$(_this).combobox('options').valueField;
+                  for (var i = 0; i < datas.length; i++)
+				{
+					if(datas[i][valueField]==vals[0])
+					{
+						itemselect=datas[i];
+						break;
+					}
+				}			
+				if(itemselect!=null)
+				 $(_this).combobox('options').onSelect.call($('#' + id)[0], itemselect);
+            }
+             
+        },
+        loadData: function (data) {
+            var _this = $(this);
+            $(_this).combobox("loadData", data);
+        },
+        options: function () {
+            var _this = $(this);
+            return $(_this).combobox("options");
+        },
+        getValue: function () {
+            var _this = $(this);
+            var re = $(_this).combobox("getValue");
+            return re == undefined ? "" : re;
+        },
+        getValues: function () {
+            var _this = $(this);
+            var re = $(_this).combobox("getValues");
+            return re == undefined ? [] : re;
+        },
+        getData: function () {
+            var _this = $(this);
+            var re = $(_this).combobox("getData");
+            return re == undefined ? [] : re;
+        }
+    };
+
+    $.fn.DropDropRadio = function (method) {
+        // 方法调用
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + 'does not exist on mutiselect.js');
+        }
+    };
+})(jQuery);
