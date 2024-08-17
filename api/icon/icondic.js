9d88495a7c3455c51aa7fae106bbf17b650dfb3a
wanghc <wanghuicai@imedway.com>
2024-08-17 12:29:06 +0800

    新增`临床药理实验患者`图标 [4865322]

--- api/icon/icondic.js
+++ api/icon/icondic.js
@@ -607,6 +607,7 @@ var iconDictionary={
     'icon-quality':"质控，质量",
     'icon-internationalize':'翻译',
     'icon-mass-injury':'群伤',
+    'icon-gcp':'临床药理实验患者',
     "icon-big-open-file":"资料归档，文件打开",
     "icon-big-waxblock-return":"蜡块归档",
     "icon-big-cells-smear":"细胞学涂片",
